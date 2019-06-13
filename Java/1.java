15e268f14bba23f55a762d4cb8a7f30c28a93011
YouwangDeng <dengyouwang@buaa.edu.cn>
2019-06-13 09:51:50 +0800

    modify two sum solution

--- Java/1.java
+++ Java/1.java
@@ -1,16 +1,13 @@
 Quesiton Description:
 
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-```java
 Example:
 
-Given nums = [2, 7, 11, 15], target = 9,
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-```
+    Given nums = [2, 7, 11, 15], target = 9,
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
 
 __________________________________________________________________________________________________
 2ms
@@ -64,7 +61,7 @@ class Solution {
             if (map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i)
                 return new int[] {i, map.get(target - nums[i])};
         }
-     throw new IllegalArgumentException("No soln");   
+     throw new IllegalArgumentException("No solution");   
     }
 }
 __________________________________________________________________________________________________
